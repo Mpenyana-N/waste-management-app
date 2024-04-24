3e40d4036a4c6f61bf5580394476898e73a1eb68
Mpenyana-N <141871651+Mpenyana-N@users.noreply.github.com>
2024-04-24 15:09:57 +0200

    Add files via upload

---
+++ src/main/java/com/enviro/assessment/grad001/nickmpenyana/wastemanagementapp/WasteManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.nickmpenyana.wastemanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagementAppApplication.class, args);
+	}
+
+}
