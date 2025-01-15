cc320807aa48176c7c65baf0f89632385faa2762
Mpenyana-N <nickmpenyana@gmail.com>
2025-01-15 10:39:28 +0200

    Initial commit

---
+++ waste-management-app/src/main/java/com/enviro/assesment/grad001/nickmpenyana/waste_management_app/WasteManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assesment.grad001.nickmpenyana.waste_management_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteManagementAppApplication.class, args);
+	}
+
+}
---
+++ waste-management-app/src/test/java/com/enviro/assesment/grad001/nickmpenyana/waste_management_app/WasteManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assesment.grad001.nickmpenyana.waste_management_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasteManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
